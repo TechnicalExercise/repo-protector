157642704d36e6c971fdf6b7b25387ce87180a3d
Assaf Stone <asstone@microsoft.com>
2022-05-01 20:26:00 -0400

    add branch creation if repo is empty

--- BranchProtectorFunc/BranchProtector/run.ps1
+++ BranchProtectorFunc/BranchProtector/run.ps1
@@ -3,6 +3,8 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
+Import-Module .\Modules\GitHubAccess.psm1
+
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed the request."
 
@@ -16,7 +18,7 @@ if ($TriggerMetadata) {
         $repo = $TriggerMetadata.repository.name
         $owner = $TriggerMetadata.repository.owner.login
         $branch = $TriggerMetadata.repository.default_branch
-        $senderLogin = $TriggerMetadata.login
+        $senderLogin = $TriggerMetadata.sender.login
 
         Write-Host "Setting policy for $owner/$repo : $branch"
 
@@ -24,6 +26,9 @@ if ($TriggerMetadata) {
 
         Write-Host "Branch protection template: $protectionTemplate"
 
+        Set-DefaultBranchContent -Username $senderLogin -Token $secret -Owner $owner -Repo $repo
+
+        $responseCode = Set-BranchProtection -Username $senderLogin -Token $secret -Owner $owner -Repo Template -Branch main -ProtectionTemplate $protectionTemplate
     }
 }
 
@@ -49,48 +54,3 @@ Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode = [HttpStatusCode]::OK
         Body       = $body
     })
-
-    function CreateGitHubRequestHeaders([string]$username, [string]$token){
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$token)))
-        $headers = @{Authorization="Basic $base64AuthInfo"}
-        return $headers
-    }
-    
-    function GetRestfulErrorResponse($exception) {
-        $ret = ""
-        if($exception.Exception -and $exception.Exception.Response){
-            $result = $exception.Exception.Response.GetResponseStream()
-            $reader = New-Object System.IO.StreamReader($result)
-            $reader.BaseStream.Position = 0
-            $reader.DiscardBufferedData()
-            $ret = $reader.ReadToEnd()
-            $reader.Close()
-        }
-        if($ret -eq $null -or $ret.Trim() -eq ""){
-            $ret = $exception.ToString()
-        }
-        return $ret
-    }
-    
-    function Get-BranchProtection {
-        [CmdletBinding()]
-        param (
-            [Parameter(Mandatory=$true)][String]$Username,
-            [Parameter(Mandatory=$true)][String]$Token,
-            [Parameter(Mandatory=$true)][String]$Owner,
-            [Parameter(Mandatory=$true)][String]$Repo,
-            [Parameter(Mandatory=$true)][String]$Branch
-        )
-    
-        try {
-            $headers = CreateGitHubRequestHeaders -username $Username -token $Token
-            Invoke-WebRequest -Method Get `
-                -Uri "https://api.github.com/repos/$Owner/$Repo/branches/$Branch/protection" `
-                -Headers $headers
-        } catch {
-            $resp = (GetRestfulErrorResponse $_)
-            Write-Error $resp
-            throw
-        }
-    }
